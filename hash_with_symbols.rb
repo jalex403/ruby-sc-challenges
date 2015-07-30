6a5be6f16c5e298bd0ef6c965cb529483e1d50e9
jalex403 <jalex403@gmail.com>
2015-07-29 23:48:12 -0400

    1st commit on new branch

---
+++ hash_with_symbols.rb
@@ -0,0 +1,7 @@
+my_favorites = {"book" => "The art of war", "movie" => "The Matrix", "song" => "Hotel California", "food" => "Bacon"}
+
+my_favorites_symbols = {book: "The art of war", movie: "The Matrix", song: "Hotel California", food: "Bacon"}
+
+puts "My favorite stuff is #{my_favorites["book"]}, #{my_favorites["movie"]}, #{my_favorites["song"]}, #{my_favorites["food"]}"
+
+puts "My favorite stuff with symbols is #{my_favorites_symbols[:book]}, #{my_favorites_symbols[:movie]}, #{my_favorites_symbols[:song]}, #{my_favorites_symbols[:food]}"
